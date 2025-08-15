03636f250a40e6a6f88201f7a41ac24d0c2cc316
Perry Huang <perryhuang2015@gmail.com>
2025-08-14 21:47:37 -0400

    feat: add literally everything almost

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,108 @@
-import { useState, useEffect } from "react";
+import Image from "./Image.jsx";
+import "../styles/App.css";
+import { useState } from "react";
 
-function App() {}
+function App() {
+    let [score, setScore] = useState(0);
+    let [isClicked, setIsClicked] = useState({
+        sableye: false,
+        "giratina-origin": false,
+        pikachu: false,
+        "mimikyu-disguised": false,
+        "mimikyu-busted": false,
+        metagross: false,
+        gliscor: false,
+        "giratina-altered": false,
+        ferrothorn: false,
+        gengar: false,
+    });
+    return (
+        <>
+            <p>{score}</p>
+            <div className="container">
+                <Image
+                    name="sableye"
+                    key="sableye"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="giratina-origin"
+                    key="giratina-origin"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="pikachu"
+                    key="pikachu"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="mimikyu-disguised"
+                    key="mimikyu-disguised"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="mimikyu-busted"
+                    key="mimikyu-busted"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="metagross"
+                    key="metagross"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="gliscor"
+                    key="gliscor"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="giratina-altered"
+                    key="giratina-altered"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="ferrothorn"
+                    key="ferrothorn"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+                <Image
+                    name="gengar"
+                    key="gengar"
+                    score={score}
+                    setScore={setScore}
+                    isClicked={isClicked}
+                    setIsClicked={setIsClicked}
+                />
+            </div>
+        </>
+    );
+}
 
 export default App;
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,16 +1,61 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import "../styles/Image.css";
 
-export default function Image({ name }) {
+export default function Image({
+    name,
+    score,
+    setScore,
+    isClicked,
+    setIsClicked,
+}) {
     let [data, setData] = useState(null);
-    async function getData() {
+    let [pokeName, setPokename] = useState("");
+    async function getData(pokemon) {
         const response = await fetch(
-            "https://pokeapi.co/api/v2/pokemon/ditto/",
+            "https://pokeapi.co/api/v2/pokemon/" + pokemon,
         );
         const result = await response.json();
-        setData(result.sprites.front_default);
+        setData(result);
+        function cleanName(fullName) {
+            let cleanedName = "";
+            for (let i = 0; i < fullName.length; ++i) {
+                if (i === 0) {
+                    cleanedName = cleanedName.concat(fullName[i].toUpperCase());
+                } else if (fullName[i] === "-") {
+                    cleanedName = cleanedName.concat(" ");
+                } else if (fullName[i - 1] === "-") {
+                    cleanedName = cleanedName.concat(fullName[i].toUpperCase());
+                } else {
+                    cleanedName = cleanedName.concat(fullName[i]);
+                }
+            }
+            return cleanedName;
+        }
+        setPokename(cleanName(result.name));
     }
-    useEffect(() => {
-        getData();
-    }, []);
-    return <img src={data} />;
+    useMemo(() => {
+        getData(name);
+    }, [name]);
+    function handleScore() {
+        if (isClicked[name] === true) {
+            setScore(0);
+            let allFalse = {};
+            for (let key in isClicked) {
+                allFalse[key] = false;
+            }
+            setIsClicked(allFalse);
+        } else {
+            setScore(score + 1);
+            setIsClicked({
+                ...isClicked,
+                [name]: true,
+            });
+        }
+    }
+    return (
+        <button className="card" onClick={handleScore}>
+            <img src={data && data.sprites.front_default} />
+            <p>{pokeName}</p>
+        </button>
+    );
 }
